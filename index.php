1de1dd7e1e713b5e7f4934aabf5e3b903bbdbbd8
Curtis Parham <parhamcurtis@gmail.com>
2021-07-30 22:24:00 -0500

    finished the auto loader and now we can autoload our files in

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php 
 session_start();
+use \Core\Config;
 
 //define constants 
 define('PROOT', __DIR__);
 define('DS', DIRECTORY_SEPARATOR);
 
+
+spl_autoload_register(function($className){
+    $parts = explode('\\', $className);
+    $class = end($parts);
+    array_pop($parts);
+    $path = strtolower(implode(DS, $parts));
+    $path = PROOT . DS . $path . DS . $class . '.php';
+    if(file_exists($path)) {
+        include($path);
+    }
+});
+
+$dbName = Config::get('db_name');
+var_dump($dbName);
